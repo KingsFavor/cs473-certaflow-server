705ab7fea9c3626c41e73a50e8c27d73f95b46e9
KingsFavor <rnjsdn1003@kaist.ac.kr>
2023-12-02 23:23:38 +0900

    first commit

---
+++ src/main/java/com/cs473/cs473server/Cs473ServerApplication.java
@@ -0,0 +1,13 @@
+package com.cs473.cs473server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs473ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs473ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs473/cs473server/Cs473ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs473.cs473server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs473ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
